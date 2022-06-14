fc33e9177c093930a282d469bf902bbd04492f79
Aza223 <0627744@schoolsnet.act.edu.au>
2022-06-14 14:13:08 +1000

    Final Summit

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,15 +1,21 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(int) var countdownMax
+var currentTimer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	currentTimer=countdownMax
+	$HUD/Countdown.text = str(currentTimer)
 
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("Game Over")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	for button in $"Layout/Main/Buttons/GameScenes".get_children():
+		button.connect("pressed", self, "_on_Button_Pressed", [button.scene_to_load])
+
+func _on_Button_Pressed(scene_to_load):
+	print(scene_to_load)
+	get_tree().change_scene(scene_to_load)
